bb136e64ba316e70994a465ecf025136155046de
mainadennis <mainajjm0@gmail.com>
2024-10-01 19:55:07 +0300

    Configure Routing in App.js

--- ngeni-frontend/src/App.js
+++ ngeni-frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApolloProviderComponent } from "./graphql/client";
+import LandingPage from "./components/LandingPage";
+import RegisterPage from "./components/RegisterPage";
+import LoginPage from "./components/LoginPage";
+import VerifyPage from "./components/VerifyPage";
+import ResetPasswordPage from "./components/ResetPasswordPage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProviderComponent>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/verify" element={<VerifyPage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </ApolloProviderComponent>
   );
 }
 
