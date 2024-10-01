2d601f07f87880e84acd2829978a2369c70a81dd
mainadennis <mainajjm0@gmail.com>
2024-10-01 19:58:45 +0300

    Create a simple dashboard component

---
+++ ngeni-frontend/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+// src/components/Dashboard.js
+import React from "react";
+import { Typography } from "@mui/material";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Typography variant="h4">Dashboard</Typography>
+      <Typography variant="body1">Welcome to your dashboard!</Typography>
+    </div>
+  );
+};
+
+export default Dashboard;
