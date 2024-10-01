492233d00b3ef2106e05da92fe2644da1792588f
mainadennis <mainajjm0@gmail.com>
2024-10-01 19:55:57 +0300

    Create a Basic landing page component

---
+++ ngeni-frontend/src/components/LandingPage.js
@@ -0,0 +1,22 @@
+// src/components/LandingPage.js
+import React from "react";
+import { Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Typography variant="h3">Welcome to Ngenu!</Typography>
+      <Button variant="contained" onClick={() => navigate("/register")}>
+        Register
+      </Button>
+      <Button variant="contained" onClick={() => navigate("/login")}>
+        Login
+      </Button>
+    </div>
+  );
+};
+
+export default LandingPage;
